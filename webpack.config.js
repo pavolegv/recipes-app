546ac58643dffc77dda5bfd535b1bec3007d18ce
Oleg Pavlenko <pavolegv@gmail.com>
2022-04-13 15:31:55 +0300

    second

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,32 @@ module.exports = {
         filename: "bundle.js"
     },
     module: {
-        rules: [{ test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }],
+        rules: [{ test: /\.js$/, exclude: /node_modules/, use: { loader: "babel-loader"} }, ],
     }
 };
+
+/*const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack'); //to access built-in plugins
+
+module.exports = {
+    mode: 'development',
+    entry: path.resolve(__dirname, 'src/index.js'),
+    output: {
+        path: path.resolve(__dirname, 'output'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [{
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }]
+    }
+};
+*/
