2aa1d74b6377a8042d1cd79e7c806d1f250454ad
oleg <job.o.pavlenko@gmail.com>
2022-04-13 19:59:29 +0300

    huynya

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 var path = require("path");
 module.exports = {
-    entry: "./src/index.js",
+    entry: ["./src/index.js"],
+//            "./src/data/recipes.json" ],
     output: {
         path: path.join(__dirname, "dist", "assets"),
         filename: "bundle.js"
     },
     module: {
-        rules: [{ test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }]
+        rules: [{ test: /\.js$|jsx/, exclude: /node_modules/, loader: "babel-loader" }],
     }
 };
